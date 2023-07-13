dd841af14e8004c78f612aa81153f1813aeb0d13
Amy KWong <amykwong6@gmail.com>
2023-07-13 18:07:38 -0400

    JS - assignmet - 7/13/23- unit 2 block 13

---
+++ index.js
@@ -0,0 +1,391 @@
+// Is thruthy
+
+//make a variable and let it  = true
+//make a if/else statement
+//if result is falsy make it spit out the following
+
+// Input Values	                     Expected Print
+// "I am a string"	                     true
+// false	                             "The boolean value false is falsy"
+// null	                                 "The null value is falsy"
+// undefined	                         "undefined is falsy"
+// 0	                                 "The number 0 is falsy (the only falsy number)"
+// ""	                                 "The empty string is falsy (the only falsy string)"
+
+
+// let iamastring = true;
+
+// if (iamastring) {
+//     console.log ("truthy")
+// }
+//     else {
+//         console.log ("The boolean value false is falsy")
+//     };
+
+
+// let false = false;
+
+// if (false) {
+//     console.log ("truthy")
+// }
+//     else {
+//         console.log ("The boolean value false is falsy")
+//     };
+
+// let Null = false;
+
+// if (Null) {
+//     console.log ("truthy")
+// }
+//     else {
+//         console.log ("The null value is falsy")
+//     };
+
+// let Undefined = false;
+
+// if (Undefined) {
+//      console.log ("truthy")
+// }
+//     else {
+//       console.log ("undefined is falsy")
+//     };    
+
+
+// let 0 = false;
+
+// if (0) {
+//      console.log ("truthy")
+// }
+//     else {
+//       console.log ("The number 0 is falsy (the only falsy number)")
+//     };    
+
+
+// let "" = false;
+
+// if ("") {
+//      console.log ("truthy")
+// }
+//     else {
+//       console.log ( "The empty string is falsy (the only falsy string)")
+//     };    
+
+
+
+
+//number line
+
+//make it in 3 variables 
+//one of those variables is a sum of num1 and num2
+// takes the sum of two numbers and let them spit out some prints
+//make a if/else if/else statement
+
+
+
+// let Num1= 50;
+// let Num2 = 51;
+// let sum = Num1 + Num2
+
+// if (sum > 100) {
+//  console.log( sum + " is greater than 100")
+
+// }
+
+// else if (sum < 100) {
+//    console.log ( sum + " is greater than 0")
+//  }
+
+// else if (sum = 0) {
+//     console.log ( sum + " is equal to 0")
+// }
+
+// else if (sum < 0) {
+//     console.log ( sum + " is a negative number")
+// }
+
+
+// let Num1= -5
+// let Num2 = 0
+// let sum = Num1 + Num2
+
+// if (sum > 100) {
+//  console.log( sum + " is greater than 100")
+
+// }
+
+// else if (sum > 0) {
+//    console.log ( sum + " is greater than 0")
+//  }
+
+// else if (sum == 0) {
+//     console.log ( sum + " is equal to 0")
+// }
+
+// else if (sum < 0) {
+//     console.log ( sum + " is a negative number")
+// }
+
+// let Num1= -500
+// let Num2 = 500
+// let sum = Num1 + Num2
+
+// if (sum > 100) {
+//  console.log( sum + " is greater than 100")
+
+// }
+
+// else if (sum > 0) {
+//    console.log ( sum + " is greater than 0")
+//  }
+
+// else if (sum == 0) {
+//     console.log ( sum + " is equal to 0")
+// }
+
+// else if (sum < 0) {
+//     console.log ( sum + " is a negative number")
+// }
+
+
+// let Num1= -1000
+// let Num2 = 0
+// let sum = Num1 + Num2
+
+// if (sum > 100) {
+//  console.log( sum + " is greater than 100")
+
+// }
+
+// else if (sum > 0) {
+//    console.log ( sum + " is greater than 0")
+//  }
+
+// else if (sum == 0) {
+//     console.log ( sum + " is equal to 0")
+// }
+
+// else if (sum < 0) {
+//     console.log ( sum + " is a negative number")
+// }
+
+
+// let Num1= 101
+// let Num2 = 0
+// let sum = Num1 + Num2
+
+// if (sum > 100) {
+//  console.log( sum + " is greater than 100")
+
+// }
+
+// else if (sum > 0) {
+//    console.log ( sum + " is greater than 0")
+//  }
+
+// else if (sum == 0) {
+//     console.log ( sum + " is equal to 0")
+// }
+
+// else if (sum < 0) {
+//     console.log ( sum + " is a negative number")
+// }
+
+// let Num1= 99
+// let Num2 = -2
+// let sum = Num1 + Num2
+
+// if (sum > 100) {
+//  console.log( sum + " is greater than 100")
+
+// }
+
+// else if (sum > 0) {
+//    console.log ( sum + " is greater than 0")
+//  }
+
+// else if (sum == 0) {
+//     console.log ( sum + " is equal to 0")
+// }
+
+// else if (sum < 0) {
+//     console.log ( sum + " is a negative number")
+// }
+
+
+
+
+
+//greather than 5
+
+//make it in 3 variables that have a number greater or equal to 5.
+//put those vaiables in ()
+//print true if true and false if false
+//make a if/else statement
+
+
+
+
+// let a = 10;
+// let b = 11;
+// let sum = a + b
+
+// if (a + b) {
+//     console.log (true)
+// }
+
+// else if (sum < 5) {
+//     console.log ( sum + "false")
+// }
+
+
+
+// let a = 5;
+// let b = 6;
+// let sum = a + b
+
+// if (a + b) {
+//     console.log (true)
+// }
+
+// else if (sum < 5) {
+//     console.log ( sum + "false")
+// }
+
+
+// let a = 0;
+// let b = 0;
+// let sum = a + b
+
+// if (a + b) {
+//     console.log (true)
+// }
+
+// else if (sum < 5) {
+//     console.log ( sum + "false")
+// }
+
+// let a = 1000;
+// let b = -1000;
+// let sum = a + b
+
+// if (a + b) {
+//     console.log (true)
+// }
+
+// else if (sum < 5) {
+//     console.log ( sum + "false")
+// }
+
+// let a = 5;
+// let b = 5;
+// let sum = a + b
+
+// if (a + b) {
+//     console.log (true)
+// }
+
+// else if (sum < 5) {
+//     console.log ( sum + "false")
+// }
+
+
+//pair and compare
+
+//make variables based on the values the assignment gave us
+//param1A	             param1B	           param2A	           Param2B	     Expected Print
+// "cat"	              "cat"	                6	               "6"	             true 
+// "five"	               5	                "dog"	          "dawg"	         false
+// 0	                   false	            "horse"  	      "horse"	         true
+// "eight"	               "eight"	            "ate"	          "ate"	             true
+// 11	                   "eleven"	            "four"	          "for"	             false
+// "cake"	               "cake"	            "pie"	          "pie"	             true
+//compares two sets of two value using strict equality operators (a===b - compare both value and type)
+//make it print true if at least one of the pairs is truthy.
+//make a if/else statement
+
+
+let C= "Cat"
+let Si= 6
+let Numbf= 5
+let F= "Five"
+let D= "Dog"
+let Dw= "Dawg"
+let Z= 0
+let Fa= "False"
+let H= "Hourse"
+let Nume= 8
+let Ee = "eight"
+let A= "Ate"
+let NumL= 11
+let E= "Eleven"
+let Ff= "Four"
+let Vf= "Forr"
+let K= "Cake"
+let P= "Pie"
+
+//console.log( null === undefined ); // false
+
+// if (C===C) {
+//   console.log(true)
+// }
+
+// else if (Si===Si){
+//     console.log (true)
+// }
+  
+// else {
+//     console.log (false)
+// }
+
+
+
+// if (F===Numbf) {
+//     console.log(true)
+//   }
+  
+//   else if (D===Dw){
+//       console.log (true)
+//   }
+    
+//   else {
+//       console.log (false)
+//   }
+
+
+//   if (Z===Fa) {
+//     console.log(true)
+//   }
+  
+//   else if (H===H){
+//       console.log (true)
+//   }
+    
+//   else {
+//       console.log (false)
+//   }
+
+
+//   if (Ee===Ee) {
+//     console.log(true)
+//   }
+  
+//   else if (A===A){
+//       console.log (true)
+//   }
+    
+//   else {
+//       console.log (false)
+//   }
+
+
+  if (K===K) {
+    console.log(true)
+  }
+  
+  else if (P===P){
+      console.log (true)
+  }
+    
+  else {
+      console.log (false)
+  }
